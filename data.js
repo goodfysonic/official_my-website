27951946893760c71bed4db4139eeac3d7da221b
cuonglexuan203 <cuonglexuan203@gmail.com>
2023-10-09 11:36:44 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,43 @@
+// Add data into this file to render into your page
+const jsonData = {
+    cards: [
+        {
+            type: "Exercise - WP",
+            title: "Week 4",
+            description: "Emailist",
+            link: "https://lxc-portfolio.vercel.app/projects/9",
+        },
+        {
+            type: "Exercise - WP",
+            title: "Week 3",
+            description: "Murach survey",
+            link: "https://lxc-portfolio.vercel.app/projects/8",
+        },
+        {
+            type: "Exercise - WP",
+            title: "Week 5",
+            description: "Murach survey",
+            link: "https://lxc-portfolio.vercel.app/projects/8",
+        },
+        {
+            type: "Exercise - WP",
+            title: "Week 6",
+            description: "Murach survey",
+            link: "https://lxc-portfolio.vercel.app/projects/8",
+        },
+        {
+            type: "Exercise - WP",
+            title: "Week 7",
+            description: "Murach survey",
+            link: "https://lxc-portfolio.vercel.app/projects/8",
+        },
+        {
+            type: "Exercise - WP",
+            title: "Week 8",
+            description: "Murach survey",
+            link: "https://lxc-portfolio.vercel.app/projects/8",
+        },
+    ],
+};
+
+export default jsonData;
